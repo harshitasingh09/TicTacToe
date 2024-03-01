489bb9404335580a7bddd02b9ee4c05e23ab4024
harshita <harshita.singh@mcbirdtechnologies.com>
2024-03-02 02:34:13 +0530

    game functionality added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,15 +1,59 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Sqaure from "./Sqaure";
+import iconX from "../assets/icon_X.jpg";
+import icon0 from "../assets/icon_O.jpg";
 
 const Board = () => {
-  const [boardState] = useState(Array(9).fill(null));
+  const [boardState, setBoardState] = useState(Array(9).fill(null));
+  const [isXTurn,setIsXTurn]=useState(false);
   const handleClick = (index) => {
-    console.log(index); 
+    const copyState = [...boardState];
+    if (boardState[index] || CheckWinner()) {
+      return;
+    }
+    copyState[index] = isXTurn ? (
+      <img style={{ maxWidth: "50px" }} src={iconX} key={1} />
+    ) : (
+      <img style={{ maxWidth: "50px" }} src={icon0} key={2} />
+    );
+    console.log(copyState[index]);
+    console.log("copyState[index]", copyState);
+    setBoardState(copyState);
+    setIsXTurn(!isXTurn);
   };
+  const CheckWinner = () => {
+    const WinnerLogic = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let logic of WinnerLogic) {
+      const [a, b, c] = logic;
 
+      console.log("boardState", boardState);
+      if (
+        boardState[a]?.key &&
+        boardState[a]?.key === boardState[b]?.key &&
+        boardState[a]?.key === boardState[c]?.key
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+  const isWinner = CheckWinner();
   return (
-   
+    <>
+      {isWinner ? (
+        ` winner`
+      ) : (
         <>
+          {" "}
           <div className="board-container">
             <div className="board-row">
               <Sqaure value={boardState[0]} onClick={() => handleClick(0)} />
@@ -28,9 +72,9 @@ const Board = () => {
             </div>
           </div>
         </>
-  )
-   
-  
+      )}
+    </>
+  );
 };
 
 export default Board;
